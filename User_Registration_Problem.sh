7a2dc83f772a14df95237b2198073894d4e9ebe0
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-02-14 15:19:53 +0530

    ADDED: Function To Validate First Name

--- User_Registration_Problem.sh
+++ User_Registration_Problem.sh
@@ -1,2 +1,29 @@
 #!/bin/bash -x
 echo "Welcome To User Registration Problem"
+
+#Function To validation
+function validation()
+{
+   letter=$1
+   pattern=$2
+   if [[ $letter =~ $pattern ]]
+   then
+      echo "$letter is Valid!!"
+      break
+   else
+      echo "Oops!!.$letter is invalid...please Re-enter it!!"
+   fi
+}
+
+#Function To Validate First Name
+function firstNameValidation()
+{
+   fName=^[A-Z]{1}[a-z]{2,}$
+   while true
+   do
+      read -p "Enter First Name : " firstName
+      validation $firstName $fName
+   done
+}
+firstNameValidation
+
