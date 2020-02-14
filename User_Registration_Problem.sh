8719032da90e85005d8c3d1f849974bb4e5d332c
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-02-14 17:32:30 +0530

    ADDED: Validate To Exactly One Special Character

--- User_Registration_Problem.sh
+++ User_Registration_Problem.sh
@@ -79,7 +79,7 @@ function mobileNumberValidation()
 #Function To Validate Password
 function passwordValidation()
 {
-   password="^[A-Z]{1,}[0-9]{1,}[A-Za-z0-9+._-%]{6,}$"
+   password="^[A-Z]{1,}[0-9]{1,}[+_-%@]{1}[A-Za-z0-9+._-%]{5,}$"
 	while true
    do
       read -p "Enter Password : " pass
