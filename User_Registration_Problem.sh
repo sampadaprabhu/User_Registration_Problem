4efa64c4950b151eb740653dc6664e8cef2181a2
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-02-14 17:19:46 +0530

    ADDED: Validate Password To At Least One Upper Case Letter

--- User_Registration_Problem.sh
+++ User_Registration_Problem.sh
@@ -25,7 +25,7 @@ function firstNameValidation()
       validation $firstName $fName
    done
 }
-firstNameValidation
+#firstNameValidation
 
 #Function To Validate Last Name
 function lastNameValidation()
@@ -37,7 +37,7 @@ function lastNameValidation()
       validation $lastName $lName
    done
 }
-lastNameValidation
+#lastNameValidation
 
 #Function To Validate Email Address
 function emailAddressValidation()
@@ -49,7 +49,7 @@ function emailAddressValidation()
       validation $emailAddress $emailId
    done
 }
-emailAddressValidation
+#emailAddressValidation
 
 #Funtion To Matching Pattern With Given Input
 function validationForMobileNumber()
@@ -74,13 +74,13 @@ function mobileNumberValidation()
       validationForMobileNumber $mobileNumber $mobileNo
    done
 }
-mobileNumberValidation
+#mobileNumberValidation
 
 #Function To Validate Password
 function passwordValidation()
 {
-   password=^[A-Za-z0-9+._-%]{8,}$
-   while true
+   password="^[A-Z]{1,}[A-Za-z0-9+._-%]{7,}$"
+	while true
    do
       read -p "Enter Password : " pass
       validation $pass $password
